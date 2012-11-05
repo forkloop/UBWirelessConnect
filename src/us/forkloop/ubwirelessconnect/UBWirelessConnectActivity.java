fde41e2c78d73b4db3b419ee6351b1de3b5ababf
forkloop <forkloop@gmail.com>
2012-11-05 00:17:50 -0500

    Set up the framework.

---
+++ src/us/forkloop/ubwirelessconnect/UBWirelessConnectActivity.java
@@ -0,0 +1,66 @@
+package us.forkloop.ubwirelessconnect;
+
+import us.forkloop.ubwirelessconnect.R;
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RemoteViews;
+
+public class UBWirelessConnectActivity extends Activity implements OnClickListener {
+
+    private static final String PREFS_FILE = "ub_wireless_connect";
+    private static final String PREFIX = "ubwc";
+    private EditText username;
+    private EditText password;
+
+    private int appWidgetId;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ub_wireless_connect_config);
+
+        setResult(RESULT_CANCELED);
+        appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+
+        username = (EditText) findViewById(R.id.ub_name);
+        password = (EditText) findViewById(R.id.password);
+        Button confirmButton = (Button) findViewById(R.id.confirm_button);
+        confirmButton.setOnClickListener(this);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.confirm_button) {
+            // Save user info.
+            SharedPreferences.Editor editor = getSharedPreferences(PREFS_FILE, MODE_PRIVATE).edit();
+            editor.putString(PREFIX + "username", username.getText().toString());
+            editor.putString(PREFIX + "password", password.getText().toString());
+            editor.commit();
+
+            // Update widget.
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+            RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.ub_wireless_connect_widget);
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+            Intent intent = new Intent();
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+}
---
+++ src/us/forkloop/ubwirelessconnect/UBWirelessConnectWidgetProvider.java
@@ -0,0 +1,13 @@
+package us.forkloop.ubwirelessconnect;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class UBWirelessConnectWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
